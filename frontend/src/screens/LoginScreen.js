0da81be5954209dabc9ed4c5dccbac7fb37a29c7
Uğur Can Altun <82045097+ugurcanaltun@users.noreply.github.com>
2022-12-08 21:53:14 +0300

    implemented login page

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,47 +1,82 @@
 import * as React from 'react';
-import { useState } from 'react';
-import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import {Link} from 'react-router-dom';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
 import { setUserTypeReducer } from '../redux/reducers/userTypeSlice';
 
+const theme = createTheme();
+
 export default function LoginScreen() {
-    const dispatch = useDispatch()
-    const [name, setName] = useState("")
-
-    const handleChange = event => {
-        setName(event.target.value)
-    };
-
-    const handleStudentButton = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
         const userState = {
-            username: name,
-            userType: "student"
+            username: data.get('username'),
+            userType: data.get('userRole')
         }
         dispatch(setUserTypeReducer(userState))
-    };
-
-    const handleCoordinatorButton = () => {
-        const userState = {
-            username: name,
-            userType: "coordinator"
-        }
-        dispatch(setUserTypeReducer(userState))
+        navigate("/home")
     };
 
     return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline/>
-            <TextField id="username" label="username" variant="outlined" onChange={handleChange} value={name}/>
-            <Link to="home">
-                <Button variant="contained" onClick={handleStudentButton}>Student</Button>
-            </Link>
-            <Link to="home">
-                <Button variant="contained" onClick={handleCoordinatorButton}>Coordinator</Button>
-            </Link>
-        </Box>
-    )
+        <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+            >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Name and Surname"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                />
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="userRole"
+                label="User Role"
+                id="userRole"
+                autoComplete="user-role"
+                />
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                >
+                Sign In
+                </Button>
+            </Box>
+            </Box>
+        </Container>
+        </ThemeProvider>
+  );
 }
