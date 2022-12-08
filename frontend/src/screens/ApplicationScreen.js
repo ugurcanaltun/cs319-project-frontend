d9ecdce7cb1f351f86c2be30e006350707e20309
Uğur Can Altun <82045097+ugurcanaltun@users.noreply.github.com>
2022-12-09 00:33:22 +0300

    applications screen finished

--- frontend/src/screens/ApplicationScreen.js
+++ frontend/src/screens/ApplicationScreen.js
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import StyledTable from '../components/StyledTable';
 
 export default function ApplicationsScreen() {
+    const headers = [
+        ["#", "Semester", "Semester Startdate", "Semester Enddate", "Status", "Application Status", "Form Code", "Operations"]
+    ];
+
+    const rows = [
+        ["1"]
+    ];
     return (
-        <h2>ApplicationsScreen</h2>
+        <>
+        <h1>Applications</h1>
+        <h2>Erasmus Program (KA103)</h2>
+        <StyledTable headers={headers} rows={rows}/>
+        </>
     )
 }
