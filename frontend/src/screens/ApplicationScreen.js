870a297eb350c39b71bb1b9d85874dfd3a49c066
kbkabadayi <kbkabadayi7@gmail.com>
2022-12-16 21:53:52 +0300

    worst button ever

--- frontend/src/screens/ApplicationScreen.js
+++ frontend/src/screens/ApplicationScreen.js
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import StyledTable from '../components/StyledTable';
+import Button from '@mui/material/Button';
 
 export default function ApplicationsScreen() {
     const headers = [
-        ["#", "Semester", "Semester Startdate", "Semester Enddate", "Status", "Application Status", "Form Code", "Operations"]
+        ["#", "Host University", "Semester", "Semester Start Date", "Semester End Date", "Status", "Application Status", "Form Code", "Operations"]
     ];
 
     const rows = [
         ["1"]
     ];
 
-    const width = 800;
+    const width = 1200;
 
     return (
         <>
-        <h1>Applications</h1>
-        <h2>Erasmus Program (KA103)</h2>
-        <StyledTable width={width} headers={headers} rows={rows}/>
+            <h1>Applications</h1>
+            <h2>Erasmus Program (KA103)</h2>
+            <StyledTable width={width} headers={headers} rows={rows} />
+
+            <Button sx={{ backgroundColor: "#035206", marginTop: 3, marginRight: 2 }} align="right" variant="contained">Accept Invitation</Button>
+            <Button sx={{ backgroundColor: "#751C08", marginTop: 3 }} align="right" variant="contained">Cancel Application</Button>
         </>
     )
 }
