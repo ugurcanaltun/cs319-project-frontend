f8115c085138830285ec07a0f28868c0805edfe2
kbkabadayi <kbkabadayi7@gmail.com>
2022-12-17 19:18:56 +0300

    apislice

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -3,12 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
-  tagTypes: ['User', 'Tasks', 'Transcripts', 'Syllabus'],
+  tagTypes: ['User', 'Tasks', 'Transcripts', 'Syllabus', 'ScoreTable', 'Wish', 'WishList', 'PreApproval'],
   endpoints: (builder) => ({
     getUser: builder.query({
       query: () => '/erasmus/getUser/1',
       providesTags: ['User'],
     }),
+
+    // TASK
     getTasks: builder.query({
       query: () => '/erasmus/1/getAllTasks',
       providesTags: ['Tasks'],
@@ -22,18 +24,15 @@ export const apiSlice = createApi({
       invalidatesTags: ['Tasks'],
     }),
     deleteTask: builder.mutation({
-<<<<<<< HEAD
       query: (id) => ({
         url: `users/tasks/${id}`,
-=======
-      query: ({ id }) => ({
-        url: `/erasmus/1/tasks/remove/${id}`,
->>>>>>> main
         method: 'DELETE',
         body: id
       }),
       invalidatesTags: ['Tasks'],
     }),
+
+    // ISO
     submitTranscript: builder.mutation({
       query: (transcript) => ({
         url: `transcripts`,
@@ -42,6 +41,16 @@ export const apiSlice = createApi({
       }),
       invalidatesTags: ['Transcripts'],
     }),
+    uploadScoreTable: builder.mutation({
+      query: (scoreTable) => ({
+        url: `uploadScoreTable/`,
+        method: 'POST',
+        body: scoreTable
+      }),
+      invalidatesTags: ['ScoreTable'],
+    }),
+
+    // WISHLIST LOGIC
     uploadSyllabus: builder.mutation({
       query: (syllabus) => ({
         url: `uploadSyllabus/`,
@@ -50,6 +59,88 @@ export const apiSlice = createApi({
       }),
       invalidatesTags: ['Syllabus'],
     }),
+    addWish: builder.mutation({
+      query: (wish) => ({
+        url: `addWish`,
+        method: 'POST',
+        body: wish,
+      }),
+      invalidatesTags: ["Wish"],
+    }),
+    submitWishList: builder.mutation({
+      query: (wishList) => ({
+        url: `submitWishList`,
+        method: 'POST',
+        body: wishList
+      }),
+      invalidatesTags: ["WishList"],
+    }),
+
+    // PreApproval
+    getPreApprovalStatus: builder.query({
+      query: () => '/amed/amed',
+      providesTags: ['PreApprovalStatus']
+    }),
+    submitPreApproval: builder.mutation({
+      query: () => ({
+        url: `submitPreApproval`,
+        method: `POST`,
+      }),
+      invalidatesTags: ["PreApproval"]
+    }),
+    downloadPreApproval: builder.query({
+      query: () => '/amed',
+      providesTags: ['PreApproval']
+    }),
+    getAllPreApprovals: builder.query({
+      query: () => '/amedamedamed',
+      providesTags: ['PreApproval']
+    }),
+    updatePreApproval: builder.mutation({
+      query: () => ({
+        url: "memedov",
+        method: "POST",
+      }),
+      invalidatesTags: ['PreApproval'],
+    }),
+
+    // Learning Agreement
+    downloadLearningAgreement: builder.query({
+      query: () => 'amedamed',
+      providesTags: ['LearningAgreement'],
+    }),
+    uploadLearningAgreement: builder.mutation({
+      query: () => ({
+        url: 'aahhhhhhhhhhhhhhhhhhmetttttttttttttttttttttttttttt',
+        invalidatesTags: ['LearningAgreement']
+      }),
+    }),
+
+    // Course Proposals
+    getCourseProposals: builder.query({
+      query: () => 'amed',
+      providesTags: ['CourseProposals']
+    }),
+    updateCourseProposal: builder.mutation({
+      query: () => ({
+        url: 'amed',
+        method: "POST",
+      }),
+      invalidatesTags: ['LearningAgreement']
+    }),
+
+    // Course Transfers
+    getCourseTransfers: builder.query({
+      query: () => 'amud',
+      providesTags: ['CourseTransfer'],
+    }),
+    updateCourseProposal: builder.mutation({
+      query: () => 'mumed',
+      invalidatesTags: ['CourseTransfer'],
+    }),
+
+
+
   }),
 })
 
